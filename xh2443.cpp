b527998b25ce05809e16092c749aac7a0d5b39a8
xh2443 <xh2443@columbia.edu>
2019-09-16 17:50:55 -0400

    commit1

--- xh2443.cpp
+++
@@ -1,4 +0,0 @@
-// Author: xh2443
-// returns e raised to the given power, minus one (ex-1)
-// Rename this file to xllexpm1.cpp and implement XLL.EXPM1 which calls expm1
-// Add documentation and a test function that runs in debug mode.
---
+++ xllexpm1.cpp
@@ -0,0 +1,38 @@
+// Author: xh2443
+// returns e raised to the given power, minus one (ex-1)
+// Rename this file to xllexpm1.cpp and implement XLL.EXPM1 which calls expm1
+// Add documentation and a test function that runs in debug mode.
+#include "xllfloat.h"
+
+#ifndef CATEGORY
+#define CATEGORY "Float"
+#endif
+
+using namespace xll;
+
+static AddInX xai_expm1(
+	FunctionX(XLL_DOUBLE, _T("?xll_expm1"), _T("XLL.EXPM1"))
+	.Arg(XLL_DOUBLE, _T("x"), _T("is the floating."))
+	.Category(CATEGORY)
+	.FunctionHelp(_T("Computes e raised to the given power, minus one."))
+	.Documentation(LR"("Computes e raised to the given power, minus one."
+	)")
+);
+double WINAPI
+xll_expm1(double x)
+{
+#pragma XLLEXPORT
+
+	return exp(x) - 1;
+}
+
+#if _DEBUG
+
+xll::test test_xll_expm1([]() {
+	double presult;
+	double param = 1;
+	presult = xll_expm1(param);
+	ensure(presult == 1);
+	});
+
+#endif // _DEBUG
