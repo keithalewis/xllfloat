b84731dd144998584d9122ae7581e665bce40c7e
tianling shen <schoolfield@126.com>
2019-09-17 09:46:28 -0400

    rename ts3157.cpp to xlltanh.cpp

--- ts3157.cpp
+++
@@ -1,4 +0,0 @@
-// Author: ts3157
-// hyperbolic tangent
-// Rename this file to xlltanh.cpp and implement XLL.TANH which calls tanh
-// Add documentation and a test function that runs in debug mode.
