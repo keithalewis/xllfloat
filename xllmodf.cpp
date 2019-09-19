de29cdf737873a1bc4a84a9e718d928c0f4e234e
LZ2735 <55263722+LZ2735@users.noreply.github.com>
2019-09-19 06:35:08 -0400

    Add files via upload

---
+++ xllmodf.cpp
@@ -0,0 +1,43 @@
+// Author: lz2735
+// decomposes a number into integer and fractional parts
+// Rename this file to xllmodf.cpp and implement XLL.MODF which calls modf
+// Add documentation and a test function that runs in debug mode.
+#include "xllfloat.h"
+
+#ifndef CATEGORY
+#define CATEGORY _T("Float")
+#endif
+
+using namespace xll;
+static AddIn xai_modf(
+	Function(XLL_DOUBLE, _T("?xll_modf"), _T("MODF"))
+	.Arg(XLL_DOUBLE, _T("x"), _T("is a floating point number."))
+	.Category(CATEGORY)
+	.FunctionHelp(_T("Decomposes a number into integer and fractional parts"))
+	.Documentation(LR"(
+        The _modf functions return a value that is the integer part of the number and another value that is the fractional part of the number. There is no error return.
+    )")
+);
+double WINAPI
+*xll_modf(double x)
+{
+#pragma XLLEXPORT
+	double intpart, fractpart;
+	double result[2];
+	fractpart = modf(x, &intpart);
+	result[0] = intpart;
+	result[1] = fractpart;
+	return result;
+}
+#if _DEBUG
+
+xll::test test_xll_modf([]() {
+	double A[2];
+	double param = 2.35;
+	*A = *xll_modf(param);
+	ensure((A[0]==2)&&(A[1]==0.35));
+	});
+#endif
+
+
+
