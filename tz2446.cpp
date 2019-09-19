82f68f8984e29e6dd140e113791a141ac9f7c70b
Tianyi Zhang <zhang.4695@outlook.com>
2019-09-19 15:49:16 -0400

    submit hw2 final version

--- tz2446.cpp
+++
@@ -1,4 +0,0 @@
-// Author: tz2446
-// nearest integer, rounding away from zero in halfway cases
-// Rename this file to xllround.cpp and implement XLL.ROUND which calls round
-// Add documentation and a test function that runs in debug mode.
