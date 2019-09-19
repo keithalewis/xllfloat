9a819bc8f20525c2f57fc937c801bf5c4cfaa9f4
miaow1 <mw3302@columbia.edu>
2019-09-19 18:56:32 -0400

    Update xllerfc.cpp

--- xllerfc.cpp
+++ xllerfc.cpp
@@ -23,12 +23,12 @@ _FP12* WINAPI
 xll_erfc(double x)
 {
 #constant
-  double a1 =  0.254829592;
-  double a2 = -0.284496736;
-  double a3 =  1.421413741;
-  double a4 = -1.453152027;
-  double a5 =  1.061405429;
-  double p  =  0.3275911;
+  double alpha1 =  0.2548295;
+  double alpha2 = -0.2844967;
+  double alpha3 =  1.4214137;
+  double alpha4 = -1.4531520;
+  double alpha5 =  1.0614054;
+  double beta  =  0.3275911;
 
 
   int sign = 1;
@@ -37,10 +37,10 @@ xll_erfc(double x)
   x = fabs(x);
 
 
-  double t = 1.0/(1.0 + p*x);
-  double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
-
-  return 1.0-sign*y;
+  double z = 1.0/(1.0 + beta*x);
+  double y = 1.0 - (((((alpha5*z + alpha4)*z) + alpha3)*z + alpha2)*t + alpha1)*z*exp(-x*x);
+  double anw=1.0-sign*y
+  return anw;
 }
 
 #if _DEBUG
