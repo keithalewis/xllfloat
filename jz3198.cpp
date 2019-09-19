a6e8769ba0d444c9c2ee44be89522405eb96ca79
俊旻 朱 <Zjm875437118@gmail.com>
2019-09-20 07:57:25 +0800

    Homework2 by Junmin Zhu

--- jz3198.cpp
+++ jz3198.cpp
@@ -2,3 +2,28 @@
 // checks if the given number is negative
 // Rename this file to xllsignbit.cpp and implement XLL.SIGNBIT which calls signbit
 // Add documentation and a test function that runs in debug mode.
+#include "xllfloat.h"
+
+#ifndef CATEGORY
+#define CATEGORY _T("Float")
+#endif
+
+using namespace xll;
+
+static AddIn xai_signbit(
+	Function(XLL_DOUBLE, _T("?xll_signbit"), _T("SIGNBIT"))
+	.Arg(XLL_DOUBLE, _T("x"), _T("is a floating point number."))
+	.Category(CATEGORY)
+	.FunctionHelp(_T("Reverses the sign of x by calling _signbit()"))
+	.Documentation(LR"(
+        The signbit functions return a value that's equal to the floating-point argument x, 
+        Nonzero integral value if arg is negative, 0 otherwise. There is no error return.
+    )")
+);
+double WINAPI
+xll_signbit(double x)
+{
+#pragma XLLEXPORT
+
+	return signbit(x);
+}
