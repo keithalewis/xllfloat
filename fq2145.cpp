8ed275efb9a58f341b4346aa8c21d9624254c544
Fei Q <feiwqiu@gmail.com>
2019-09-19 19:15:13 -0400

    hw2

--- fq2145.cpp
+++
@@ -1,4 +0,0 @@
-// Author: fq2145
-// arc tangent, using signs to determine quadrants
-// Rename this file to xllatan2.cpp and implement XLL.ATAN2 which calls atan2
-// Add documentation and a test function that runs in debug mode.
