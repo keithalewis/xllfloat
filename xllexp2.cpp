7a187f2a1cc9e76cc60ffd6e9ddc992163b7a612
Yizhou Gu <1842191969@qq.com>
2019-09-19 00:44:34 -0400

    Pierce

---
+++ xllexp2.cpp
@@ -0,0 +1,41 @@
+// Author: yg2707
+// returns 2 raised to the given power (2x)
+// Rename this file to xllexp2.cpp and implement XLL.EXP2 which calls exp2
+// Add documentation and a test function that runs in debug mode.
+
+#include "xllfloat.h"
+
+#ifndef CATEGORY
+#define CATEGORY _T("Float")
+#endif
+
+using namespace xll;
+
+static AddIn xai_exp2(
+	Function(XLL_DOUBLE, _T("?xll_exp2"), _T("XLL.EXP2"))
+	.Arg(XLL_DOUBLE, _T("x"), _T("is a floating point number."))
+	.Category(CATEGORY)
+	.FunctionHelp(_T("return exp2(x) by calling _exp2()"))
+	.Documentation(LR"(
+        The _exp2 functions return a value that's equal to exp2(x). There will be no error return.
+    )")
+);
+double WINAPI
+xll_exp2(double x)
+{
+#pragma XLLEXPORT
+
+	return exp2(x);
+}
+
+#if _DEBUG
+
+xll::test test_xll_exp2([]() {
+
+	double presult;
+	double x = 4;
+	presult = xll_exp2(x);
+	ensure(presult == 16);
+	});
+
+#endif // _DEBUG
