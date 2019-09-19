fd92db68fffde50c77bb3c05014dae8389a65bdf
scarlett <scarlett@SHIJIAFUBA08>
2019-09-19 15:44:19 -0400

    Correct version

--- xllfmin.cpp
+++ xllfmin.cpp
@@ -13,9 +13,9 @@
 using namespace xll;
 
 static AddInX xai_fmin(
-	FunctionX(XLL_FP, _T("?xll_fmin"), _T("XLL.FMIN"))
-	.Arg(XLL_DOUBLE, _T("x"), _T("is the first input floating point value."))
-	.Arg(XLL_DOUBLE, _T("y"), _T("is the second input floating point value."))
+	FunctionX(XLL_DOUBLE, _T("?xll_fmin"), _T("XLL.FMIN"))
+	.Arg(XLL_DOUBLE, _T("x"), _T("is the first input floating point value."), _T("1.1"))
+	.Arg(XLL_DOUBLE, _T("y"), _T("is the second input floating point value."), _T("3.22"))
 	.Category(CATEGORY)
 	.FunctionHelp(_T("Computes the smaller of two floating point values."))
 	.Documentation(LR"(
@@ -28,16 +28,15 @@ double WINAPI
 xll_fmin(double x, double y) 
 {
 #pragma XLLEXPORT
-	return std::fmin(x, y);
+	return std::min(x, y);
 }
 
 #if _DEBUG
 
 xll::test test_xll_fmin([]() {
-	double result;
 	double x = 1.1;
 	double y = 3.22;
-	result = xll_fmin(x, y);
+	double result = xll_fmin(x, y);
 	ensure(fabs(result-1.1)<=1e-6);
 });
 
