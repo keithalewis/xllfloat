ca3ccd878dc8757e877dfca739b4086f374d845d
Chloe Chen <jc5302@columbia.edu>
2019-09-26 16:09:43 -0400

    jc5302

--- fmsroot1d.h
+++ fmsroot1d.h
@@ -51,18 +51,62 @@ namespace fms::root1d {
 	}
 
 	// Secant: 0 = m(x - x0) + y0, where m = (y1 - y0)/(x1 - x0) and yi = f(xi), i = 0,1.
-	// Return x = !!!insert formula here
+	// Return x = !!!insert formula here    x2 = (x0 * y1 - x1 * y0) / (y1 - y0)
 	inline double secant(const F& f, double x0, double x1)
 	{
-		return 0; //!!! implement formula for x
+		double y0 = f(x0);
+		if (y0 == 0)
+			return x0;
+
+		double y1 = f(x1);
+		if (y1 == 0)
+			return x1;
+
+		//ensure(y0 != copysign(y0, y1)); // bracketed
+
+		double x2 = (x0 * y1 - x1 * y0) / (y1 - y0);
+		double y2 = f(x2);
+
+		return x2;//!!! implement formula for x
 	}
 
 	inline std::pair<double,double> false_position(const F& f, double x0, double x1)
 	{
-		double x2 = secant(f, x0, x1);
+		ensure(x0 <= x1);
 
-		return std::pair(0, 0); //!!! return the values that bracket the root
+		if (x0 == x1)
+			return std::pair(x0, x1);
+
+		std::pair<double, double> p;
+
+		double y0 = f(x0);
+		if (y0 == 0)
+			return std::pair(x0, x0);
+
+		double y1 = f(x1);
+		if (y1 == 0)
+			return std::pair(x1, x1);
+
+		ensure(y0 != copysign(y0, y1)); // bracketed
+
+
+		double x2 = (x0 * y1 - x1 * y0) / (y1 - y0);
+
+		double y2 = f(x2);
+
+		if (y0 != copysign(y0, y2)) {
+			p = std::pair(x0, x2);
+		}
+		else {
+			ensure(y1 != copysign(y1, y2));
+
+			p = std::pair(x2, x1);
+		}
+		return p; //!!! return the values that bracket the root
 	}
 
 #pragma warning(pop)
 } // namespace fms::root1d
+
+
+
