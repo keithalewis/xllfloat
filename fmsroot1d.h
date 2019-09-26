646dca0231c9fb60a1e2223b53a6ae6994fa6374
celinedamogu <55509088+celinedamogu@users.noreply.github.com>
2019-09-26 14:53:57 -0400

    update

--- fmsroot1d.h
+++ fmsroot1d.h
@@ -5,6 +5,7 @@
 #include <utility>
 #include "xll12/xll/ensure.h"
 
+
 namespace fms::root1d {
 
 	// Alias for a function taking a double and returning double.
@@ -18,7 +19,7 @@ namespace fms::root1d {
 	// Given a function, f, and bracket x0 < x1 return the next bracket
 	inline std::pair<double,double> bisect(const F& f, double x0, double x1)
 	{
-		ensure(x0 <= x1);
+	ensure(x0 <= x1);
 
 		if (x0 == x1)
 			return std::pair(x0, x1);
@@ -50,19 +51,56 @@ namespace fms::root1d {
 		return p;
 	}
 
-	// Secant: 0 = m(x - x0) + y0, where m = (y1 - y0)/(x1 - x0) and yi = f(xi), i = 0,1.
-	// Return x = !!!insert formula here
+
+	//Secant: 0 = m(x - x0) + y0, where m = (y1 - y0)/(x1 - x0) and yi = f(xi), i = 0,1.
 	inline double secant(const F& f, double x0, double x1)
 	{
-		return 0; //!!! implement formula for x
+		double y0 = f(x0);
+		double y1 = f(x1);
+
+		ensure(y1 != y0);//otherwise denominator == 0
+
+		double x = (x0 * y1 - x1 * y0) / (y1 - y0);
+
+		return x;
 	}
 
-	inline std::pair<double,double> false_position(const F& f, double x0, double x1)
+	//false_position
+	inline std::pair<double, double> false_position(const F& f, double x0, double x1)
 	{
-		double x2 = secant(f, x0, x1);
+		ensure(x0 <= x1);
 
-		return std::pair(0, 0); //!!! return the values that bracket the root
+		if (x0 == x1)
+			return std::pair(x0, x1);
+
+		std::pair<double, double> p;
+
+		double y0 = f(x0);
+		if (y0 == 0)
+			return std::pair(x0, x0);
+
+		double y1 = f(x1);
+		if (y1 == 0)
+			return std::pair(x1, x1);
+
+		ensure(y0 != copysign(y0, y1)); // bracketed
+
+		double x2 = secant(f,x0,x1);
+		double y2 = f(x2);
+
+		if (y0 != copysign(y0, y2)) {
+			p = std::pair(x0, x2);
+		}
+		else {
+			ensure(y1 != copysign(y1, y2));
+
+			p = std::pair(x2, x1);
+		}
+
+		return p;
 	}
+
+
 
 #pragma warning(pop)
 } // namespace fms::root1d
--- hw3.cpp
+++ hw3.cpp
@@ -34,8 +34,8 @@ _FP12* WINAPI xll_bisect(HANDLEX f, double x0, double x1)
 	try {
 		ensure(x0 <= x1);
 
-		std::function<double(double)> F = [f](double x) { 
-			return Excel(xlUDF, OPER(f), OPER(x)); 
+		std::function<double(double)> F = [f](double x) {
+			return Excel(xlUDF, OPER(f), OPER(x));
 		};
 		std::pair<double, double> p = bisect(F, x0, x1);
 
@@ -55,18 +55,42 @@ _FP12* WINAPI xll_bisect(HANDLEX f, double x0, double x1)
 
 // See https://www.hpl.hp.com/hpjournal/pdfs/IssuePDFs/1979-12.pdf Fig. 6 page 24.
 
-static AddIn xai_kahan_a(
-	Function(XLL_DOUBLE, L"?xll_kahan_a", L"KAHAN.A")
-	.Arg(XLL_DOUBLE, L"x", L"is the argument.")
-	.Category(L"XLL")
-	.FunctionHelp(L"Evaluate 6 x - x^4 - 1.")
+static AddIn xai_false_position(
+	Function(XLL_FP, L"?xll_false_position", L"FALSE_POSITION")
+	.Arg(XLL_HANDLE, L"f", L"is the function.")
+	.Arg(XLL_DOUBLE, L"x0", L"is the lower bracket.")
+	.Arg(XLL_DOUBLE, L"x1", L"is the upper bracket.")
+	.Category(L"ROOT1D")
+	.FunctionHelp(L"Return the next bracket for the root.")
 );
-double WINAPI xll_kahan_a(double x)
+_FP12* WINAPI xll_false_position(HANDLEX f, double x0, double x1)
 {
 #pragma XLLEXPORT
-	return 6 * x - pow(x, 4) - 1;
+	static xll::FP12 result(1, 2); // 1 x 2 array of doubles
+
+	try {
+		ensure(x0 <= x1);
+
+		std::function<double(double)> F = [f](double x) {
+			return Excel(xlUDF, OPER(f), OPER(x));
+		};
+		std::pair<double, double> p = false_position(F, x0, x1);
+
+		result[0] = p.first;
+		result[1] = p.second;
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+
+		return 0; // shows as #NUM! in Excel
+	}
+
+	return result.get();
 }
 
-//!!! Implement KAHAN.B for Fig. 6 example (b) 	
 
-//!!! Implement KAHAN.C for Fig. 6 example (c) 	
+
+
+
+
+
